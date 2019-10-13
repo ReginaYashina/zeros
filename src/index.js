c76267ecdbd7a2e128b9913ab0dec702afef0e14
ReginaYashina <reginayashina@inbox.ru>
2019-10-13 17:51:56 +0300

    feat: add function zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 module.exports = function zeros(expression) {
-  // your solution
+let arr = expression.split('*');
+let elem = 0;
+let count_2 = 0;
+let count_5 = 0;
+let minus;
+
+
+for(elem; elem<arr.length; elem++){
+    if(arr[elem].includes('!!')){
+        minus = 2;
+    }
+    else if(arr[elem].includes('!') && !(arr[elem].includes('!!'))){
+        minus = 1;
+    }
+    let num = parseInt(arr[elem]);
+
+    while(num>0){
+        if(!(num%2) && num >= 2){
+            let a = num;
+            while(!(a % 2))
+            {
+                a = a / 2;
+                count_2 = count_2 + 1;
+            }
+        }
+
+        if(!(num%5) && num >= 5){
+
+            let b = num;
+            while(!(b % 5))
+            {
+                b = b / 5;
+                count_5 = count_5 + 1;
+            }
+        }
+        num=num-minus;
+    }
 }
+
+if(count_2<count_5){
+    return count_2;
+}else{
+    return count_5;
+}
+}
+
+
